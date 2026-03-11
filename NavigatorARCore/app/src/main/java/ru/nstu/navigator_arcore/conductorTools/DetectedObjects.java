8e007759f7c4bf6ff217260500b7b97ecc7d8c13
IIalexandrII <sasha.shunkov@mail.ru>
2026-03-12 00:42:40 +0700

    Удалена папка NavigatorARCore из main (перенесена в executorch)

--- NavigatorARCore/app/src/main/java/ru/nstu/navigator_arcore/conductorTools/DetectedObjects.java
+++
@@ -1,7 +0,0 @@
-package ru.nstu.navigator_arcore.conductorTools;
-
-public class DetectedObjects {
-    public String name;
-    public int id;
-    public String description;
-}
--- NavigatorARCore/app/src/main/java/ru/nstu/navigator_arcore/conductorTools/VibrationPatterns.java
+++
@@ -1,7 +0,0 @@
-package ru.nstu.navigator_arcore.conductorTools;
-
-public class VibrationPatterns {
-    public VibrationPatterns(){}
-    public long[] timings;
-    public int[] amplitudes;
-}
